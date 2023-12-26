9e2f7cf0c79c2b8eea60c98e1c8cc703eea8fd77
rajon-git <rajon.zhsust15@gmail.com>
2023-12-26 23:29:59 +0600

    product card add and product json added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screen/HomeScreen';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Header/>
       <main className="py-3">
         <Container>
-        <h1>Wellcome</h1>
+          <HomeScreen/>
         </Container>
       
       </main>
