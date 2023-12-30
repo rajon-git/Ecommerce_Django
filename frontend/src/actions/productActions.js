2ab9618c7ce004e186eedfb98fde332a2ca1fe61
rajon-git <rajon.zhsust15@gmail.com>
2023-12-30 22:33:59 +0600

    bring redux to home screen dispatch and bring state

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,7 +5,7 @@ import {
     PRODUCT_LIST_FAILED
 } from '../constants/productConstants';
 
-const listProducts =()=>async(dispatch)=>{
+export const listProducts =()=>async(dispatch)=>{
     try {
         dispatch({type: PRODUCT_LIST_REQUEST})
         const {data} = await axios.get('/api/products/')
