f1b285b23904950edcaaae8f6fad61c68245f4ad
rajon-git <rajon.zhsust15@gmail.com>
2023-12-31 23:23:06 +0600

    loader component and message component added

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -3,6 +3,8 @@ import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import { useDispatch, useSelector } from 'react-redux'
 import { listProducts } from '../actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 function HomeScreen() {
   const dispatch = useDispatch();
@@ -11,13 +13,13 @@ function HomeScreen() {
 
   useEffect(()=>{
     dispatch(listProducts())
-  },[])
+  },[dispatch])
 
   return (
     <div>
       <h1>Latest Products</h1>
-      {loading ? <h2>Loading... </h2>
-      :error ? <h3>{error}</h3>
+      {loading ? <Loader/>
+      :error ? <Message variant='danger'>{error}</Message>
       :
       <Row>
         {products.map((product) =>(
