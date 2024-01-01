57e13bc1a1abc6ad93740d4adc14317da4c0391e
rajon-git <rajon.zhsust15@gmail.com>
2024-01-01 22:01:17 +0600

    cart constant->reducer->store->actions added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,12 +2,20 @@ import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {thunk} from 'redux-thunk';
 import {composeWithDevTools} from '@redux-devtools/extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 const reducer =combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
 });
 
-const initialState={};
+
+const cartItemFromLocalStorage= localStorage.getItem('cartItems') ?
+         JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState={
+    cart: {cartItems: cartItemFromLocalStorage}
+};
 const middleware= [thunk];
 
 const store = createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)));
