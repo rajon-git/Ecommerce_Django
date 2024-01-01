dafbe5bde5324b67c96b7b13b5809c066f2ab8e8
rajon-git <rajon.zhsust15@gmail.com>
2024-01-01 22:31:49 +0600

    cart action dispatch and state in cartscreen

--- frontend/src/screen/CartScreen.js
+++ frontend/src/screen/CartScreen.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useParams } from 'react-router-dom';
+import { addCart } from '../actions/cartActions';
 
 function CartScreen() {
+    const dispatch = useDispatch();
+    const cart = useSelector(state => state.cart);
+    const {cartItems} = cart;
+    console.log('cartItems: ', cartItems);
+    const { id } = useParams();
+    const location = useLocation();
+    const qty = Number(location.search.split('=')[1]) || 1;
+    
+
+    useEffect(()=>{
+        if (id) {
+            dispatch(addCart(id,qty))
+        } else {
+            
+        }
+    },[dispatch,id,qty])
   return (
     <div>
       CART
