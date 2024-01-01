2d02e3087e8653a47c37e3aaf70fff9541e13c5b
rajon-git <rajon.zhsust15@gmail.com>
2024-01-01 21:05:31 +0600

    qty added

--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
-import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
+import { Button, Card, Col, Form, Image, ListGroup, Row } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import { useDispatch, useSelector } from 'react-redux';
 import { listProductDetails } from '../actions/productActions';
@@ -8,6 +8,7 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 
 function ProductScreen() {
+    const [qty,setQty] = useState(1);
     const dispatch= useDispatch();
     const { id } = useParams();
     const productDetails=useSelector((state)=> state.productDetails);
@@ -15,7 +16,7 @@ function ProductScreen() {
 
     useEffect(()=>{
     dispatch(listProductDetails(id));
-  },[dispatch]);
+  },[dispatch,id]);
     return (
     <div>
         <Link to="/" className='btn btn-light my-3'>Go Back</Link>
@@ -68,8 +69,30 @@ function ProductScreen() {
                         </Row>
                     </ListGroup.Item>
 
+                    {product.countInStock > 0 && (
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Qty</Col>
+                                <Col xs='auto' className='my-1'>
+                                   <Form.Control
+                                   as="select"
+                                   value={qty}
+                                   onChange={(e)=> setQty(e.target.value)}
+                                   >
+                                    {[...Array(product.countInStock).keys()].map((x) =>
+                                    (
+                                        <option key={x + 1} value={x + 1}>
+                                            {x + 1}
+                                        </option>
+                                    ))}
+                                   </Form.Control>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    )}
+
                     <ListGroup.Item>
-                        <Button className='btn-block' disabled={product.countInStock == 0} type='button'>Add to Cart</Button>
+                        <Button className='btn-block' disabled={product.countInStock === 0} type='button'>Add to Cart</Button>
                     </ListGroup.Item>
                 </ListGroup>
             </Card>
