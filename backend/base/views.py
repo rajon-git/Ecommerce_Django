2426a284cf48c887bd6bbb42d834fff0ced765e8
rajon-git <rajon.zhsust15@gmail.com>
2024-01-03 00:38:59 +0600

    user serializer added

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,8 +3,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serailizers import ProductSerializer
-from .products import products
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 # Create your views here.
@@ -14,22 +13,30 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self,attrs):
         data = super().validate(attrs)
 
-        data['username'] = self.user.username
-        data['email'] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
         
         return data
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getProducts(request):
     products=Product.objects.all()
-    serailizer = ProductSerializer(products, many=True)
-    return Response(serailizer.data)
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
-    serailizer = ProductSerializer(product, many=False)
-    return Response(serailizer.data)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
