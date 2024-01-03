2546bf6b7da21035813c0a3f064fe1b91e813b85
rajon-git <rajon.zhsust15@gmail.com>
2024-01-04 00:27:59 +0600

    register restapi add

--- backend/base/views.py
+++ backend/base/views.py
@@ -9,6 +9,9 @@ from .models import Product
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 # Create your views here.
 
 
@@ -17,13 +20,31 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         data = super().validate(attrs)
 
         serializer = UserSerializerWithToken(self.user).data
-
         for k, v in serializer.items():
             data[k] = v
         
         return data
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data 
+    try:
+        user= User.objects.create(
+            first_name = data['name'],
+            username = data['email'],
+            email = data['email'],
+            password= make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail':'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
 @api_view(['GET'])
 @permission_classes(IsAuthenticated)
