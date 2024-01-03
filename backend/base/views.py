ae9cdf43f583660a4197819ef71635581b833e39
rajon-git <rajon.zhsust15@gmail.com>
2024-01-03 23:17:10 +0600

    protectected route add

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+
+from django.contrib.auth.models import User
 from .models import Product
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -23,11 +26,18 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
+@permission_classes(IsAuthenticated)
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
+@api_view(['GET'])
+@permission_classes(IsAdminUser)
+def getUsers(request):
+    users=User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):
