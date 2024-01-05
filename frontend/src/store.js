81035062025c1396c02649c3c912b206c529ca56
rajon-git <rajon.zhsust15@gmail.com>
2024-01-05 23:15:18 +0600

    user actions create and storage update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,18 +3,24 @@ import {thunk} from 'redux-thunk';
 import {composeWithDevTools} from '@redux-devtools/extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
+
 const reducer =combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 });
 
 
 const cartItemFromLocalStorage= localStorage.getItem('cartItems') ?
          JSON.parse(localStorage.getItem('cartItems')) : []
+const userInfoFromLocalStorage= localStorage.getItem('userInfo') ?
+         JSON.parse(localStorage.getItem('userInfo')) : null
 
 const initialState={
-    cart: {cartItems: cartItemFromLocalStorage}
+    cart: {cartItems: cartItemFromLocalStorage},
+    userLogin: {userInfo: userInfoFromLocalStorage}
 };
 const middleware= [thunk];
 
