80f7e730116556e9cd6fb53648f0eacd346f00c2
rajon-git <rajon.zhsust15@gmail.com>
2024-01-10 23:21:57 +0600

    logout functionality added and also add logout actions

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,18 @@
 import React from 'react'
-import { Navbar,Nav,Container, Row } from 'react-bootstrap'
+import { Navbar,Nav,Container, Row, NavDropdown } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import {LinkContainer} from 'react-router-bootstrap'
+import { logout } from '../actions/userActions';
 function Header() {
+  const dispatch = useDispatch();
+  const userLogin = useSelector(state => state.userLogin);
+  const{userInfo} = userLogin;
+
+  const logoutHandler =(e)=>{
+    // e.preventDefault();
+    dispatch(logout());
+    console.log("logout");
+  }
   return (
     <header>
       <Navbar bg="dark" variant='dark' expand="lg"  collapseOnSelect>
@@ -16,9 +27,23 @@ function Header() {
           <LinkContainer to="/cart">
           <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/login">
-          <Nav.Link><i className='fas fa-user'></i>Login</Nav.Link>
-          </LinkContainer>
+
+          {userInfo ? (
+            <NavDropdown title= {userInfo.name} id= 'username'>
+              <LinkContainer to='/profile'>
+                <NavDropdown.Item>Profile</NavDropdown.Item>
+              </LinkContainer>
+
+              <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+            </NavDropdown>
+          ) :
+          (
+            <LinkContainer to="/login">
+            <Nav.Link><i className='fas fa-user'></i>Login</Nav.Link>
+            </LinkContainer>
+          )
+        }
+          
             
             
           </Nav>
