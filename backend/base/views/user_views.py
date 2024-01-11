0b29962acfa6c63beaf91859839555fb7b518523
rajon-git <rajon.zhsust15@gmail.com>
2024-01-11 21:55:29 +0600

    user profile update rest api create

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -39,8 +39,23 @@ def registerUser(request):
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
+#Here update user profile
 
+@api_view(['PUT'])
+@permission_classes(IsAuthenticated)
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializerWithToken(user, many=False)
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+    user.save()
+    return Response(serializer.data)
 
+#get user profile
 @api_view(['GET'])
 @permission_classes(IsAuthenticated)
 def getUserProfile(request):
@@ -48,6 +63,7 @@ def getUserProfile(request):
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
+#check user isadmin or normal user
 @api_view(['GET'])
 @permission_classes(IsAdminUser)
 def getUsers(request):
