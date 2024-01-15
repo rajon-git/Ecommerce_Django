d82323f6923cedfde7cc897ee6da48df00ef7ac1
rajon-git <rajon.zhsust15@gmail.com>
2024-01-15 23:31:42 +0600

    placeorder functionality add

--- frontend/src/screen/PlaceOrderScreen.js
+++ frontend/src/screen/PlaceOrderScreen.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import CheckoutSteps from '../components/CheckoutSteps';
-import { Col, Image, ListGroup, Row } from 'react-bootstrap';
+import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
 import Message from '../components/Message';
 import { Link } from 'react-router-dom';
 
 function PlaceOrderScreen() {
     const cart = useSelector(state => state.cart);
+
+    cart.itemsPrice = cart.cartItems.reduce((acc,item)=> acc + item.price * item.qty, 0).toFixed(2);
+    cart.shippingPrice = (cart.itemsPrice > 2000 ? 0 : 70).toFixed(2);
+    cart.taxPrice = Number((0.15) * cart.itemsPrice).toFixed(2);
+    cart.totalPrice = (Number(cart.itemsPrice) + Number(cart.shippingPrice) +  Number(cart.taxPrice)).toFixed(2);
+
+    const placeOrder=()=>{
+        console.log("place order")
+    }
   return (
     <div>
       <CheckoutSteps step1 step2 step3 step4/>
@@ -66,7 +75,53 @@ function PlaceOrderScreen() {
             </ListGroup>
         </Col>
 
-        <Col md={4}></Col>
+        <Col md={4}>
+            <Card>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h2>Order Summary</h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Row>
+                            <Col>Item: </Col>
+                            <Col>${cart.itemsPrice} </Col>
+                        </Row>
+                    </ListGroup.Item>
+
+                    <ListGroup.Item>
+                        <Row>
+                            <Col>Shipping: </Col>
+                            <Col>${cart.shippingPrice} </Col>
+                        </Row>
+                    </ListGroup.Item>
+
+                    <ListGroup.Item>
+                        <Row>
+                            <Col>Tax: </Col>
+                            <Col>${cart.taxPrice} </Col>
+                        </Row>
+                    </ListGroup.Item>
+
+                    <ListGroup.Item>
+                        <Row>
+                            <Col>Total: </Col>
+                            <Col>${cart.totalPrice} </Col>
+                        </Row>
+                    </ListGroup.Item>
+
+                    <ListGroup.Item>
+                        <Button 
+                        type='button' 
+                        className='btn-block' 
+                        disabled={cart.cartItems === 0}
+                        onClick={placeOrder}
+                        >
+                            Place Order
+                        </Button>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Card>
+        </Col>
       </Row>
     </div>
   )
