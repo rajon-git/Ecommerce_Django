f8851c129dc9a90b035004d8648826006f4f709b
rajon-git <rajon.zhsust15@gmail.com>
2024-01-15 23:08:09 +0600

    in placeorder orderitems details add and calculate that

--- frontend/src/screen/PlaceOrderScreen.js
+++ frontend/src/screen/PlaceOrderScreen.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import CheckoutSteps from '../components/CheckoutSteps';
-import { Col, ListGroup, Row } from 'react-bootstrap';
+import { Col, Image, ListGroup, Row } from 'react-bootstrap';
+import Message from '../components/Message';
+import { Link } from 'react-router-dom';
 
 function PlaceOrderScreen() {
     const cart = useSelector(state => state.cart);
@@ -33,6 +35,33 @@ function PlaceOrderScreen() {
                         <strong>Method: </strong>
                         {cart.paymentMethod}
                     </p>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                    <h2>Order Items</h2>
+                    {cart.cartItems.length === 0 ? <Message variant='info'>
+                        Your cart is empty
+                        </Message> : (
+                            <ListGroup variant='flush'>
+                                {cart.cartItems.map((item, index)=>(
+                                    <ListGroup.Item key={index}>
+                                        <Row>
+                                            <Col md={1}>
+                                            <Image src={item.image} alt={item.name} fluid rounded/>
+                                            </Col>
+
+                                            <Col>
+                                            <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                            </Col>
+
+                                            <Col md={4}>
+                                            {item.qty} X ${item.price} = {(item.qty * item.price).toFixed(2)}
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
+                        )}
                 </ListGroup.Item>
             </ListGroup>
         </Col>
