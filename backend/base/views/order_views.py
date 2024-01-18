bce7ad689ff31af5eb8af9b71be142888e07aa6c
rajon-git <rajon.zhsust15@gmail.com>
2024-01-18 23:06:56 +0600

    get order by Id views and url add

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -59,3 +59,18 @@ def addOrderItems(request):
 
         serializer = OrderSerializer(order, many = False)
         return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])    
+def getOrderById(request, pk):
+    user: request.user
+    order = Order.objects.get(_id=pk)
+
+    try:
+        if user.is_staff or order.user == user:
+            serializer = OrderSerializer(order, many = False)
+            return Response(serializer.data)
+        else:
+            return Response({'detail':'No Authorized to view to order'}, status =status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response({'detail':'Order does not exist'}, status= status.HTTP_400_BAD_REQUEST)
